83dac5c85905a4e248b33cb30b498ac7d9d0ff57
AthenaTI22 <amaralgeo22@gmail.com>
2026-06-29 19:59:45 -0300

    Criação da função adicionarTrabalho

--- filaimpressao.c
+++ filaimpressao.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MAX 100
 
@@ -72,11 +73,70 @@ int main() {
 }
 
 void adicionarTrabalho() {
-    int id;
-    char nomeArquivo[50];
-    int paginas;
-    char tipo;
-    printf("Em construcao...\n");
+    Trabalho novo;
+    int i;
+
+    printf("Digite o ID: ");
+    scanf("%d", &novo.id);
+
+    // Verifica ID duplicado na fila normal
+    for (i = 0; i < quantidadeNormal; i++) {
+        int pos = (inicioNormal + i) % MAX;
+        if (filaNormal[pos].id == novo.id) {
+            printf("ID ja existe!\n");
+            return;
+        }
+    }
+
+    // Verifica ID duplicado na fila prioritária
+    for (i = 0; i < quantidadePrioritaria; i++) {
+        int pos = (inicioPrioritaria + i) % MAX;
+        if (filaPrioritaria[pos].id == novo.id) {
+            printf("ID ja existe!\n");
+            return;
+        }
+    }
+
+    printf("Nome do arquivo: ");
+    scanf(" %[^\n]", novo.nomeArquivo);
+
+    printf("Quantidade de paginas: ");
+    scanf("%d", &novo.paginas);
+
+    printf("Tipo (N = Normal / P = Prioritario): ");
+    scanf(" %c", &novo.tipo);
+    
+    novo.tipo = toupper(novo.tipo);
+
+    if (novo.tipo != 'N' && novo.tipo != 'P') {
+        printf("Tipo invalido!\n");
+        return;
+    }
+
+    if (novo.tipo == 'N') {
+        if (quantidadeNormal == MAX) {
+            printf("Fila normal cheia!\n");
+            return;
+        }
+
+        filaNormal[fimNormal] = novo;
+        fimNormal++;
+        quantidadeNormal++;
+
+        printf("Trabalho adicionado na fila normal!\n");
+    }
+    else {
+        if (quantidadePrioritaria == MAX) {
+            printf("Fila prioritaria cheia!\n");
+            return;
+        }
+
+        filaPrioritaria[fimPrioritaria] = novo;
+        fimPrioritaria = (fimPrioritaria + 1) % MAX;
+        quantidadePrioritaria++;
+
+        printf("Trabalho adicionado na fila prioritaria!\n");
+    }
 }
 
 void processarImpressao() {
